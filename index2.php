94df3a9d0018460820d2d2484214b332815ba417
Irwansyah893 <appbisnis8@gmail.com>
2024-06-13 14:29:55 +0700

    Menambahkan file index2.php

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Latihan Remote Github</h1>
+    <h3>Muhammad Irwansyah</h3>
+    <h3>M.Yasir</h3>
+</body>
+</html>
